6f8bea7e3535c9a623df925ccd761641e0ac7010
Ricardo7173 <riki0303@outlook.es>
2024-06-07 11:05:50 -0600

    Toast y demás

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,36 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimengModule } from './primeng.module';
-import { NavbarComponent } from "./componentes/navbar/navbar.component";
-import { ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegisterComponent } from './componentes/register/register.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginComponent,
-    RegisterComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     PrimengModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    MessageService,
+    provideClientHydration(),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
